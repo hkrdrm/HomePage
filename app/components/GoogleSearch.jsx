48057f089e19ed376d01c827c32048fef5231bc2
Matt Spiers <mspiers@franklintelephone.com>
2018-02-14 08:59:03 -0600

    ready to deploy

--- app/components/GoogleSearch.jsx
+++ app/components/GoogleSearch.jsx
@@ -3,26 +3,23 @@ import React from 'react'
 
 const Search = Input.Search
 
-class GoogleSearch extends React.Component {
-  render() {
-    return (
-      <Card title="Google Search">
-        <form method="get" action="http://www.google.com/search">
-          <div className="form-item">
-            <Search
-              type="text"
-              name="q"
-              onChange={this.search}
-              placeholder="Search Google"
-            />
-          </div>
-          <div className="form-item">
-            <Button type="primary" htmlType="submit">Search</Button>
-          </div>
-        </form>
-      </Card>
-    )
-  }
+const GoogleSearch = () => {
+  return(
+    <Card title="Google Search">
+      <form method="get" action="http://www.google.com/search">
+        <div className="form-item">
+          <Search
+            type="text"
+            name="q"
+            placeholder="Search Google"
+          />
+        </div>
+        <div className="form-item">
+          <Button type="primary" htmlType="submit">Search</Button>
+        </div>
+      </form>
+    </Card>
+  )
 }
 
 export default GoogleSearch
--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -9,15 +9,15 @@ const Home = () => (
     <div className="center-col">
       <GoogleSearch />
       <CardRow imageSource="https://top10usenet.com/wp-content/uploads/2016/11/sabnzbd-logo.png">
-        <h3><a href="/">Sabnzbd Plus+</a></h3>
+        <h3><a href="http://sabnzbd.zombiepirate.pro/">Sabnzbd Plus+</a></h3>
         <p>To see everything currently downloading.</p>
       </CardRow>
       <CardRow imageSource="https://cdn.rawgit.com/Sonarr/Sonarr/develop/Logo/256.png">
-        <h3><a href="/">Sonarr</a></h3>
+        <h3><a href="http://sonarr.zombiepirate.pro/">Sonarr</a></h3>
         <p>Here to add tv shows to the auto downloader.</p>
       </CardRow>
       <CardRow imageSource="https://nzbgeek.info/images/vig-logo.png">
-        <h3><a href="/">NzbGeek</a></h3>
+        <h3><a href="https://nzbgeek.info/">NzbGeek</a></h3>
         <p>Go here for movies or tv shows Sonarr can&#39;t find.</p>
       </CardRow>
     </div>
